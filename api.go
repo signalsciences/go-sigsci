f95ff43997bc9c8aed4085e2f763db0babc815ab
John Hanrahan <jhanrahan@signalsciences.com>
2020-10-06 11:24:23 -0700

    '' is a valid value, dont omit on empty

--- api.go
+++ api.go
@@ -507,7 +507,7 @@ type UpdateSiteBody struct {
 	AgentLevel           string `json:"agentLevel,omitempty"`
 	BlockDurationSeconds int    `json:"blockDurationSeconds,omitempty"`
 	BlockHTTPCode        int    `json:"blockHTTPCode,omitempty"`
-	AgentAnonMode        string `json:"agentAnonMode,omitempty"`
+	AgentAnonMode        string `json:"agentAnonMode"`
 }
 
 // UpdateSite updates a site by name.
@@ -1827,7 +1827,7 @@ type CreateSiteBody struct {
 	Name                 string `json:"name,omitempty"`                 //Identifying name of the site
 	DisplayName          string `json:"displayName,omitempty"`          //Display name of the site
 	AgentLevel           string `json:"agentLevel,omitempty"`           //Agent action level - 'block', 'log' or 'off'
-	AgentAnonMode        string `json:"agentAnonMode,omitempty"`        //Agent IP anonimization mode - 'EU' or ''
+	AgentAnonMode        string `json:"agentAnonMode"`                  //Agent IP anonimization mode - 'EU' or ''
 	BlockHTTPCode        int    `json:"blockHTTPCode,omitempty"`        //HTTP response code to send when when traffic is being blocked
 	BlockDurationSeconds int    `json:"blockDurationSeconds,omitempty"` //Duration to block an IP in seconds
 }
